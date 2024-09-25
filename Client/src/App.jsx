525ba291e9030f205e2f8e90728e8f069c99bcbe
Mukilan Palanichamy <mukilan.p@kalvium.community>
2024-09-25 11:53:30 +0530

    Created login page and given Ui for Login

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,7 @@
 import {Routes, Route} from "react-router-dom"
 import CreateUser from "./Component/CreateUser";
 import MuseumData from "./Component/MuseumData";
+import Login from "./Component/Login";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/signup" element={<CreateUser/>}></Route>
         <Route path="/MuseumData" element={<MuseumData/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
       </Routes>
     </div>
   )
